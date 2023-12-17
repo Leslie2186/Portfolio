26344396d7e5bb76fd0d2f69cb20498a4c83b884
Leslie2186 <leslie.moraud@gmail.com>
2023-12-17 14:53:44 +0100

    Présentation

--- src/components/presentation/Presentation.jsx
+++ src/components/presentation/Presentation.jsx
@@ -2,39 +2,38 @@ import './Presentation.css';
 
 function Presentation() {
   return (
-    <div>
-        <h1>Portfolio - Leslie Pellegrin Moraud</h1>
-        <h2>Développeuse Web Fullstack</h2>
+    <div className='containPresentation'>
+        <h1>Développeuse Web Fullstack</h1>
         <div>
-            <h3>Bonjour, je m&apos;appelle Leslie, je suis développeuse web Fullstack, spécialisée en JavaScript!</h3>
+            <h2>Bonjour, je m&apos;appelle Leslie, je suis développeuse web Fullstack, spécialisée en JavaScript!</h2>
             <p>
                Etant curieuse de nature et passionnée par le développement, j’aime apprendre et me former sur de nouvelles technologies.<br/>
                Mes expériences professionnelles m’ont permis d’acquérir un ensemble de compétences et d’aptitudes que je souhaite mettre à profit dans votre entreprise.
             </p>
             <div className="competences">
                 <div className="hardSkills">
-                    <h4>Mes Hard skills :</h4>
+                    <h4>Mes Hard skills</h4>
                     <div className="containHardSkills">
                         <ul>
-                            <li>
+                            <li className='green'>
                                 HTML5
                             </li>
                             <li>
                                 CSS3
                             </li>
-                            <li>
+                            <li className='green'>
                                 JavaScript
                             </li>
                             <li>
                                 ReactJs
                             </li>
-                            <li>
+                            <li className='green'>
                                 Vite.Js
                             </li>
                             <li>
                                 Node.Js
                             </li>
-                            <li>
+                            <li className='green'>
                                 Express.Js
                             </li>
                             <li>
@@ -42,43 +41,43 @@ function Presentation() {
                             </li>
                         </ul>
                         <ul>
-                            <li>
+                            <li className='green'>
                                 Postman
                             </li>
                             <li>
                                 Wordpress
                             </li>
-                            <li>
+                            <li className='green'>
                                 Photoshop
                             </li>
                             <li>
                                 Figma
                             </li>
-                            <li>
+                            <li className='green'>
                                 Premiere
                             </li>
                             <li>
                                 Canva
                             </li>
-                            <li>
+                            <li className='green'>
                                 Trello
                             </li>
                         </ul>
                     </div>
                 </div>
                 <div className="softSkills">
-                    <h4>Mes Soft skills :</h4>
+                    <h4>Mes Soft skills</h4>
                     <div className="containSoftSkills">
                         <ul>
-                            <li>Curieuse</li>
+                            <li className='green'>Curieuse</li>
                             <li>Organisée</li>
-                            <li>Rigoureuse</li>
+                            <li className='green'>Rigoureuse</li>
                             <li>Adaptable</li>
-                            <li>Bonne communiquante</li>
+                            <li className='green'>Bonne communiquante</li>
                             <li>Bon esprit d&apos;équipe</li>
-                            <li>Souci du travail bien fait</li>
+                            <li className='green'>Souci du travail bien fait</li>
                             <li>Pédagogue</li>
-                            <li>Créative</li>
+                            <li className='green'>Créative</li>
                         </ul>
                     </div>
                 </div>
