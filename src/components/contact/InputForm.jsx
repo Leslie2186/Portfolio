af444b2d38ce1c63136476310f393b0bbb95c75d
Leslie2186 <leslie.moraud@gmail.com>
2023-12-17 14:56:43 +0100

    Fin du fomulaire de contact

---
+++ src/components/contact/InputForm.jsx
@@ -0,0 +1,10 @@
+function inputForm({ label, onChange, type, value, name }) {
+  return (
+    <label>
+      <span className="labelContactForm">{label}</span>
+      <input name={name} type={type} value={value} onChange={onChange} />
+    </label>
+  );
+}
+
+export default inputForm;
