17b043bbfcefecfd7f74d99f9bff46c2c41f0ca9
Leslie2186 <leslie.moraud@gmail.com>
2024-01-23 21:12:05 +0100

    Modifications css, ajouts projets, ajout animations

--- src/components/projets/Projets.jsx
+++ src/components/projets/Projets.jsx
@@ -5,8 +5,14 @@ import mobilesh from "../../assets/mobileSH.png";
 import tablettesh from "../../assets/tabletteSH.png";
 import pcsh from "../../assets/pcSH.png";
 import MesVacances from "../../assets/mesvacances.png";
+import lorealmobile from "../../assets/lorealmobile.png";
+import lorealtablette from "../../assets/lorealtablette.png";
+import lorealpc from "../../assets/lorealpc.png";
 import MVModifDelete from "../../assets/MVModifDelete.png";
 import MVContact from "../../assets/MVContact.png";
+import eatingmobile from "../../assets/eatingmobile.png";
+import eatingtablette from "../../assets/eatingtablette.png";
+import eatingpc from "../../assets/eatingpc.png";
 import InputForm from './InputForm';
 import './Projets.css';
 
@@ -38,6 +44,17 @@ function Projets() {
                     altTablette= "Super Hero Cards sur tablette"
                     info= "Objectif: Réaliser un site pour découvrir tous les super-héros existants, découvrir leurs caratéristiques et leurs skills. <br/> Possibilité de mise en favoris de ses super-héros préférés et d'affrontement entre deux super-héros."
                 />
+                 <InputForm 
+                    title= "L'Oréal - 'BeWith' la nouvelle Egérie"
+                    link= "/loreal"
+                    imgMobile= {lorealmobile}
+                    altMobile= "L'Oréal BeWith nouvelle Egérie sur mobile"
+                    imgDesktop= {lorealpc}
+                    altDesktop= "L'Oréal BeWith nouvelle Egérie sur pc"
+                    imgTablette= {lorealtablette}
+                    altTablette= "L'Oréal BeWith nouvelle Egérie sur tablette"
+                    info= "Objectif: Faire un site pour L'Oréal, pour lancer l'évènement 'BeWith' la nouvelle Egérie."
+                />
                 <InputForm 
                     title= "Les Villes De Mes Vacances"
                     link= "/projetVacances"
@@ -48,6 +65,17 @@ function Projets() {
                     imgTablette= {MVModifDelete}
                     altTablette= "Gestion des villes modification et suppression"
                     info= "Objectif: Faire un site pour présenter toutes les villes que j'ai visité durant mes vacances.<br/> Possibilité d'ajouter, de modifier et de supprimer une ville ou un pays."
+                />
+                <InputForm 
+                    title= "Eating Nam Nam"
+                    link= "/eating"
+                    imgMobile= {eatingmobile}
+                    altMobile= "Eating Nam Nam, le site de partage de recettes sur mobile"
+                    imgDesktop= {eatingpc}
+                    altDesktop= "Eating Nam Nam, le site de partage de recettes sur pc"
+                    imgTablette= {eatingtablette}
+                    altTablette= "Eating Nam Nam, le site de partage de recettes sur tablette"
+                    info= "Objectif: Faire un site de partage de recettes.<br/> Possibilité d'ajouter une recette ou un ingrédient."
                 />
             </ul>
         </div>
---
+++ src/components/projets/loreal/Loreal.jsx
@@ -0,0 +1,44 @@
+import lorealvideo from "../../../assets/lorealvideo.mp4";
+import html from '../../../assets/html-5.png';
+import css from '../../../assets/css-3.png';
+import js from '../../../assets/javascript.png';
+import git from '../../../assets/git.png';
+import github from '../../../assets/github.png';
+import react from '../../../assets/react.svg';
+import express from '../../../assets/express.png';
+import mysql from '../../../assets/mysql.png';
+import './Loreal.css';
+
+function Loreal() {
+  return (
+    <div className="projetLoreal">
+      <h1>L'Oréal "BeWith"</h1> 
+      <p><span>OBJECTIF : </span> Réaliser un <span>évènement</span> L'Oréal permettant de <span>redynamiser</span> les intéractions en <span>B&B</span> et en <span>B&C</span> pour le lancement de la prochaine gamme de <span>coloration</span>.
+      Ici l'évènement s'appelle "BeWith" et à pour but de trouver la nouvelle égérie de L'Oréal.</p> 
+      <p><span>DESIGN : </span> Sobre et élégant, les formes et les couleurs sont en accord avec les sites L'Oréal.</p>
+      <p><span>FONCTIONNEMENT : </span> Le site se compose d'une page d'accueil présentant le <span>concept</span> de l'évènement. Les candidates doivent  <span>s'inscrire</span> sur le formulaire à leur disposition sur le site.
+      Elles <span>postent</span> une photo au <span>naturel</span>. Par la suite, elles vont chez leurs <span>coiffeurs</span> L'Oréal le plus proche et choisissent parmis la nouvelle <span>gamme une coloration</span> et une coupe.
+      Ensuite, elles retournent sur le site pour <span>poster</span> leurs photos maquillées et <span>coiffées</span>. Les <span>admins</span> de chez L'Oréal choisissent <span>50 filles</span> à départager.
+      Les <span>visiteurs</span> du site peuvent s'inscrire pour <span>voter</span> parmis les 50 filles restantes. La grande <span>finale</span> aura lieu à Paris et sera retransmis sur la <span>chaîne Youtube</span> de L'Oréal.
+      La <span>final</span> se fera en la présence de la <span>guest</span> et égérie <span>Eva Longoria</span>.</p>
+      <div className='langagesOutils'>
+        <h3>LANGAGE ET OUTILS UTILISES</h3>
+        <ul>
+          <li><img src={html} alt="Logo HTML"/></li>
+          <li><img src={css} alt="Logo CSS"/></li>
+          <li><img src={js} alt="Logo JavaScript"/></li>
+          <li><img src={react} alt="Logo React"/></li>
+          <li><img src={express} alt="Logo Express"/></li>
+          <li><img src={mysql} alt="Logo MySQL"/></li>
+          <li><img src={git} alt="Logo Git"/></li> 
+          <li><img src={github} alt="Logo GitHub"/></li>
+        </ul>
+      </div>
+      <div className='videoSite'>
+      <video controls><source src={lorealvideo} type="video/mp4" /></video>
+      </div>
+    </div>
+  )
+}
+
+export default Loreal
